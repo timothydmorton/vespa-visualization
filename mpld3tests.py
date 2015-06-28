7c71acc94863cd441249ddce087e3ddb8e014135
Ganesh Ravichandran <gr2483@columbia.edu>
2015-06-28 19:37:20 -0400

    testing out new Flask stuff

--- mpld3tests.py
+++ mpld3tests.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import vespa
+import json
 from sklearn.datasets import load_iris
 
 import mpld3
@@ -15,8 +16,11 @@ def star_param_scatterplot():
 	x = starpop['H_mag']
 	y = starpop['distmod']
 
-	fig, ax = plt.subplots(111)
+	fig = plt.figure()
+	ax = fig.add_subplot(111, xlabel='$distance$', ylabel='$distmod$')
 
-	# the_plot = ax.plot(x,y)
-
-	return mpld3.fig_to_dict(fig)
+	the_plot = ax.plot(x,y)
+	f = open("mpld3_product.txt","w")
+	json01 = json.dumps(mpld3.fig_to_dict(fig))
+	f.write(json01)
+	f.close()
